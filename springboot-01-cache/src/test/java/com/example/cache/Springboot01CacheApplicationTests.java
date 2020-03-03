d6324d64a1ccda01c9f7272337c54db749a705b7
Kahen <KahenLee@163.com>
2020-03-03 17:48:58 +0800

    spring boot高级-缓存-基本环境搭建

--- springboot-01-cache/src/test/java/com/example/cache/Springboot01CacheApplicationTests.java
+++ springboot-01-cache/src/test/java/com/example/cache/Springboot01CacheApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.cache;
 
+import com.example.cache.bean.Employee;
+import com.example.cache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot01CacheApplicationTests {
+    @Autowired
+    EmployeeMapper employeeMapper;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Employee employee = employeeMapper.getEmpById(1);
+        System.out.println(employee);
+    }
 
 }
