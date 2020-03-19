1ee47e1d91e00e17390c239f6945655121722660
Kahen <kahenlee@163.com>
2020-03-20 00:34:36 +0800

    SpringBoot整合Spring Security

---
+++ securing-web/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -0,0 +1,16 @@
+package com.example.securingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kahen
+ */
+@SpringBootApplication
+public class SecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+
+}
