2a24a1b317e205cd0c82119a7e0ab0f8370b4dd5
Kahen <kahenlee@163.com>
2020-03-19 10:02:07 +0800

    定时任务

--- springboot-04-task/src/main/java/com/example/task/Springboot04TaskApplication.java
+++ springboot-04-task/src/main/java/com/example/task/Springboot04TaskApplication.java
@@ -3,11 +3,14 @@ package com.example.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Kahen
  * @EnableAsync 开启异步注解功能
+ * @EnableScheduling 开启基于注解的定时任务
  */
+@EnableScheduling
 @EnableAsync
 @SpringBootApplication
 public class Springboot04TaskApplication {
---
+++ springboot-04-task/src/main/java/com/example/task/service/ScheduledService.java
@@ -0,0 +1,34 @@
+package com.example.task.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Kahen
+ * @version 1.0
+ * @date 2020-03-19 9:22
+ */
+@Service
+public class ScheduledService {
+    /**
+     * A cron-like expression, extending the usual UN*X definition to include triggers
+     * on the second, minute, hour, day of month, month, and day of week.
+     * @code "0 * * * * MON-FRI"
+     */
+    // @Scheduled(cron = "0 * * * * MON-FRI")
+    // @Scheduled(cron = "0,1,2,3,4 * * * * MON-FRI") 枚举
+    // @Scheduled(cron = "1-10 * * * * MON-FRI") 区间
+    // @Scheduled(cron = "0/2 * * * * MON-FRI") 每2秒启动一次
+
+    /**
+     * 【0 0/5 14,18 * * ?】 每天14点整，和18点整，每隔5分钟执行一次
+     * 【0 15 10 ? * 1-6】 每个月的周一至周六10:15分执行一次
+     * 【0 0 2 ? * 6L】每个月的最后一个周六凌晨2点执行一次
+     * 【0 0 2 LW * ?】每个月的最后一个工作日凌晨2点执行一次
+     * 【0 0 2-4 ? * 1#1】每个月的第一个周一凌晨2点到4点期间，每个整点都执行一次；
+     */
+    @Scheduled(cron = "0/2 * * * * MON-FRI")
+    public void hello() {
+        System.out.println("hello...");
+    }
+}
